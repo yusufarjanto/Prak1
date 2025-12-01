9ebd22f5f8fb5ee346456891bb99741dd53c438a
Zarie29 <zarie705@gmail.com>
2025-12-02 00:24:24 +0700

    init

---
+++ ilham.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string arrayData[] = {"Zarie Ilham Junaedi", "241058012"};
+
+    for (int i = 0; i < 1; i++)
+    {
+        cout << "Nama = " << arrayData[0] << endl;
+        cout << "NIM = " << arrayData[1] << endl;
+    }
+}
